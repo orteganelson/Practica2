d6accdb222fb87fb1dd98382db703ee33b53f9e3
Usuario <Usuario@DESKTOP-8QHNMEU>
2020-11-12 17:59:06 -0500

    Commit-Proceso de creacion de las clases Telefono y Usuario

--- src/ups/edu/ec/modelo/Telefono.java
+++ src/ups/edu/ec/modelo/Telefono.java
@@ -5,10 +5,90 @@
  */
 package ups.edu.ec.modelo;
 
+import java.util.Objects;
+
 /**
  *
  * @author Usuario
  */
 public class Telefono {
+    private int codigo;
+    private String tipo;
+    private String numero;
+    private String operadora;
+    
+    
+    public Telefono(){                
+    }
+
+    public Telefono(int codigo, String tipo, String numero, String operadora) {
+        this.codigo = codigo;
+        this.tipo = tipo;
+        this.numero = numero;
+        this.operadora = operadora;
+    }        
+    
+    public Telefono(String numero) {
+        this.numero = numero;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getOperadora() {
+        return operadora;
+    }
+
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefono other = (Telefono) obj;
+        if (!Objects.equals(this.numero, other.numero)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     
 }
--- src/ups/edu/ec/modelo/Usuario.java
+++ src/ups/edu/ec/modelo/Usuario.java
@@ -5,10 +5,88 @@
  */
 package ups.edu.ec.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Usuario
  */
 public class Usuario {
+    private int id;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String clave;
+    private List<Telefono> listaTelefonos;
+
+    public Usuario(int id, String cedula, String nombre, String apellido, String correo, String clave) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.clave = clave;
+        listaTelefonos = new ArrayList<>();        
+    }
+    
+    public Usuario() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+            
+    public List<Telefono> getListaTelefonos() {
+        return listaTelefonos;
+    }
+    
+    
     
 }
