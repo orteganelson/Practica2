4c1eb36187f700f4619805d164315d4b33f71aa8
Usuario <Usuario@DESKTOP-8QHNMEU>
2020-11-12 20:16:03 -0500

    Commit-Proceso creacion de los controladores

--- src/ups/edu/ec/controlador/ControladorTelefono.java
+++ src/ups/edu/ec/controlador/ControladorTelefono.java
@@ -5,10 +5,26 @@
  */
 package ups.edu.ec.controlador;
 
+import java.util.List;
+import ups.edu.ec.modelo.Telefono;
+
 /**
  *
  * @author Usuario
  */
-public class ControladorTelefono {
+public class ControladorTelefono extends Controlador{
+    
+    public ControladorTelefono() {
+    }
+    
+    @Override
+    public int cargarCodigo() {
+        var copiaListaTelefonos = (List<Telefono>) List.copyOf(findAll());
+        if (copiaListaTelefonos.isEmpty()) {
+            return 1;
+        } else {
+            return copiaListaTelefonos.get(copiaListaTelefonos.size() - 1).getCodigo() + 1;
+        }
+    }
     
 }
--- src/ups/edu/ec/controlador/ControladorUsuario.java
+++ src/ups/edu/ec/controlador/ControladorUsuario.java
@@ -5,10 +5,53 @@
  */
 package ups.edu.ec.controlador;
 
+import java.util.List;
+import ups.edu.ec.modelo.Telefono;
+import ups.edu.ec.modelo.Usuario;
+
 /**
  *
  * @author Usuario
  */
-public class ControladorUsuario {
+public class ControladorUsuario extends Controlador{
+    
+    public ControladorUsuario() {
+    }
+    
+    @Override
+    public int cargarCodigo() {
+        if (findAll().size() > 0) {
+            return findAll().size() + 1;
+        } else {
+            return 1;
+        }
+
+    }
+
+    public Usuario iniciarSesion(String correo, String clave) {
+        for (var usuario : (List<Usuario>) findAll()) {
+            if (usuario.getCorreo().equals(correo) && usuario.getClave().equals(clave)) {
+                return usuario;
+
+            }
+
+        }
+        return null;
+    }
+
+    public Usuario readCedula(Usuario usuarioBuscar) {
+        var copiaListaUsuarios = (List<Usuario>) List.copyOf(findAll());
+        return copiaListaUsuarios.stream().filter(usuario -> usuario.getCedula().equals(usuarioBuscar.getCedula())).findFirst().get();
+    }
+
+    public Usuario readNumero(Telefono telefonoBuscar) {
+        for (var usuario : (List<Usuario>) findAll()) {
+            if (usuario.buscarTelefono(telefonoBuscar) != null) {
+                return usuario;
+            }
+
+        }
+        return null;
+    }
     
 }
